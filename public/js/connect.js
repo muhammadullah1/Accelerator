0b0ecbfb5d4abe067b100530ddbeabbdd5e04839
Kashif Ahmad <57842881+kashi326@users.noreply.github.com>
2023-05-02 10:04:27 +0300

    UI changes

--- public/js/connect.js
+++ public/js/connect.js
@@ -811,7 +811,7 @@ function initSocketIO() {
                 $("#praesiUpInfo").html('<div style="color:red;">Conversion failed!!! Error: ' + msg + '</div>');
             } else {
                 if (msg == "beginConversion") {
-                    $("#praesiUpInfo").html('Started conversion... <img class="loaderIcon" src="./img/logogross.png" style="width: 20%; display: block; margin:auto;">');
+                    $("#praesiUpInfo").html('Started conversion... <img class="loaderIcon" src="./img/Logo.svg" style="width: 20%; display: block; margin:auto;">');
                     writeToChat("Server", "Started conversion...");
                 } else if (msg == "successConversion") {
                     $("#praesiUpInfo").html('<div style="color:green;">Conversion done!</div>');
